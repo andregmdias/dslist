ec48accb9a9860bb309228f560bd717422fbb884
André Luiz Giannattasio Marques Dias <andregmdias@gmail.com>
2023-11-13 23:12:42 -0300

    feat: adiciona game seed

--- src/main/java/br/com/devsuperior/dlist/entities/Game.java
+++ src/main/java/br/com/devsuperior/dlist/entities/Game.java
@@ -17,7 +17,11 @@ Game {
   private String platforms;
   private Double score;
   private String imgUrl;
+
+  @Column(columnDefinition = "TEXT")
   private String shortDescription;
+
+  @Column(columnDefinition = "TEXT")
   private String longDescription;
 
   public Game() {
